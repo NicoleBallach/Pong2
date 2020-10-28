8626a78f1d9a08b0f86d158763fe4633445e48e6
NicoleBallach <59987177+NicoleBallach@users.noreply.github.com>
2020-10-27 18:59:55 -1000

    Initial commit

---
+++ src/my/pong2/GameFrame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.pong2;
+
+/**
+ *
+ * @author nici
+ */
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+    
+    GamePanel panel;
+    
+    /**
+     * Constructor: creates the window
+     */
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong2");
+        this.setResizable(false); //so user cannot resize the frame
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack(); //game frame class fit around game panel - we don't need to set the size of JFrame
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); //window in middle  
+    }   
+}
---
+++ src/my/pong2/Pong2Game.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.pong2;
+
+/**
+ *
+ * @author nici
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Pong2Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+    
+}
